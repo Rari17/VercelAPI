41ff7865f3697efd27dd7a091a857f2d32bbbf82
Rari17 <103605597+Rari17@users.noreply.github.com>
2024-10-29 10:27:56 +0100

    Update receive-gps-data.js

--- api/receive-gps-data.js
+++ api/receive-gps-data.js
@@ -1,16 +1,3 @@
-
-let latestGpsData = null; // Speichert die neuesten GPS-Daten
-
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const gpsData = req.body;
-
-        // GPS-Daten speichern
-        latestGpsData = gpsData;
-
-        console.log('GPS-Daten empfangen:', latestGpsData);
-        res.status(200).json({ message: 'GPS data received' });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
-    }
+export default function handler(req, res) {
+    res.status(200).json({ message: "GPS data received" });
 }
